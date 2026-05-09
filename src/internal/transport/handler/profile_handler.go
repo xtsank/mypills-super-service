7e3839d402f64b82b4ea998ab57ed39b2cab41fa
Тимофей Есин <eti23u169@student.bmstu.ru>
2026-05-09 14:56:13 +0300

    Реализован хендлер для сервиса по редактированию профиля

--- src/internal/transport/handler/profile_handler.go
+++ src/internal/transport/handler/profile_handler.go
@@ -1,1 +1,69 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/samber/do/v2"
+	"github.com/xtsank/mypills-super-service/src/internal/errors"
+	"github.com/xtsank/mypills-super-service/src/internal/service"
+	"github.com/xtsank/mypills-super-service/src/internal/service/command"
+	"github.com/xtsank/mypills-super-service/src/internal/transport/dto/req"
+	"github.com/xtsank/mypills-super-service/src/internal/transport/middleware"
+)
+
+type ProfileHandler struct {
+	profileService service.IProfileService
+}
+
+func NewProfileHandler(i do.Injector) (*ProfileHandler, error) {
+	profileService := do.MustInvoke[service.IProfileService](i)
+
+	return &ProfileHandler{profileService: profileService}, nil
+}
+
+func (h *ProfileHandler) RegisterRoutes(rg *gin.RouterGroup) {
+	cabinet := rg.Group("/profile")
+	{
+		cabinet.PATCH("/me", h.UpdateProfile)
+	}
+}
+
+// UpdateProfile godoc
+func (h *ProfileHandler) UpdateProfile(c *gin.Context) {
+	userIDValue, exists := c.Get(middleware.UserIDKey)
+	if !exists {
+		_ = c.Error(errors.ErrUnauthorized)
+		return
+	}
+	userID := userIDValue.(uuid.UUID)
+
+	var input req.UpdateProfileDto
+
+	err := c.BindJSON(&input)
+	if err != nil {
+		_ = c.Error(errors.ErrInvalidInput.WithError(err))
+		return
+	}
+
+	cmd := command.NewUpdateProfileCmd(
+		userID,
+		input.Sex,
+		input.Weight,
+		input.Age,
+		input.IsPregnant,
+		input.IsDriver,
+		input.Illnesses,
+		input.Allergies,
+	)
+
+	result, err := h.profileService.UpdateProfile(c.Request.Context(), cmd)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.Set(middleware.ResponsePayloadKey, result)
+	c.Set(middleware.ResponseStatusKey, http.StatusOK)
+}
